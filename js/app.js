0883c9c846c2f887132280e61a21931de35ed585
rosannalui <rosanna.lui123@gmail.com>
2014-12-12 22:10:51 -0800

    first commit and added first files

--- js/app.js
+++ js/app.js
@@ -1,13 +1,52 @@
-// List of Seattle Traffic Cameras
 // http://data.seattle.gov/resource/65fc-btcc.json
-
 "use strict";
 
-//put your code here to create the map, fetch the list of traffic cameras
-//and add them as markers on the map
-//when a user clicks on a marker, you should pan the map so that the marker
-//is at the center, and open an InfoWindow that displays the latest camera
-//image
-//you should also write the code to filter the set of markers when the user
-//types a search phrase into the search box
+$(document).ready(function() {
+   var mapElem = document.getElementById('map');
+   var center = {
+      lat: 47.6,
+      lng: -122.3
+   };
 
+   var map = new google.maps.Map(mapElem, {
+      center: center,
+      zoom: 12
+   });
+
+   //create new info winder
+   var infoWindow = new google.maps.InfoWindow();
+   var location;
+   var markers = [];
+
+   //gets the traffic data from seattle
+   $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+      .done(function onMarkerClick(data, itemIndex) {
+         location = data;
+         data.forEach(function(location) {
+            var marker = new google.maps.Marker({
+               position: {
+                  lat: Number(location.location.latitude),
+                  lng: Number(location.location.longitude)
+               },
+               map: map
+            });
+            markers.push(marker);
+
+            //shows a popup window when you click on a marker
+            google.maps.event.addListener(marker, 'click', function onMarkerClick() {
+               map.panTo(this.getPosition());
+               var image = '<p>' + location.cameralabel + '</p>' + '<img src= ' + location.imageurl.url + '>';
+               infoWindow.setContent(image);
+               infoWindow.open(map, this);
+            });
+
+         });
+      })
+      
+      .fail(function(error) {
+         console.log(error);
+      	})
+      
+      .always(function() {
+    	})
+});
